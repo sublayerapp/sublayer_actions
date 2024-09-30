e10e64e72830b88293f81a7558509bdfd08455d1
Scott Werner <stwerner@vt.edu>
2024-09-29 20:23:06 -0400

    Try a different model name

--- .github/workflows/daily_action_suggestion_oai/daily_action_suggestion_oai.rb
+++ .github/workflows/daily_action_suggestion_oai/daily_action_suggestion_oai.rb
@@ -9,7 +9,7 @@ Dir[File.join(__dir__, "generators", "*.rb")].each { |file| require file }
 Dir[File.join(__dir__, "agents", "*.rb")].each { |file| require file }
 
 Sublayer.configuration.ai_provider = Sublayer::Providers::OpenAI
-Sublayer.configuration.ai_model = "chatgpt-4o-latest"
+Sublayer.configuration.ai_model = "gpt-4o-2024-08-06"
 
 # Add custom Github Action code below:
 
