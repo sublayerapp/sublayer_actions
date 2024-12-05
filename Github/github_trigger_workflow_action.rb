26ed5879eaebda2fddf882fff8e4cf1c981b9255
Scott Werner <stwerner@vt.edu>
2024-12-05 05:02:59 -0500

    Creating Github/github_trigger_workflow_action.rb

---
+++ Github/github_trigger_workflow_action.rb
@@ -0,0 +1,18 @@
+class GithubTriggerWorkflowAction < GithubBase
+  def initialize(repo:, workflow_id:, inputs: {})
+    super(repo: repo)
+    @workflow_id = workflow_id
+    @inputs = inputs
+  end
+
+  def call
+    begin
+      @client.create_workflow_dispatch(@repo, @workflow_id, @inputs)
+      Sublayer.configuration.logger.log(:info, "Successfully triggered workflow \"#{@workflow_id}\" for repo \"#{@repo}\"")
+    rescue Octokit::Error => e
+      error_message = "Error triggering workflow: \#{e.message}"
+      Sublayer.configuration.logger.log(:error, error_message)
+      raise StandardError, error_message
+    end
+  end
+end
