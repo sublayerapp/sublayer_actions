45878fec42d6d559c88605acd52117984e768eb8
Scott Werner <stwerner@vt.edu>
2025-09-27 06:02:36 -0400

    Creating Google/google_calendar_create_event_action.rb

---
+++ Google/google_calendar_create_event_action.rb
@@ -0,0 +1,69 @@
+require 'google/apis/calendar_v3'
+require 'googleauth'
+
+# Description: Sublayer::Action responsible for creating events in Google Calendar.
+# This action integrates with Google Calendar API to create calendar events with specified details.
+#
+# Requires: 'google-apis-calendar_v3' and 'googleauth' gems
+# $ gem install google-apis-calendar_v3 googleauth
+# Or add to your Gemfile:
+# gem 'google-apis-calendar_v3'
+# gem 'googleauth'
+#
+# It is initialized with event details including title, description, start/end times, and optional attendees.
+# It returns the ID of the created calendar event.
+#
+# Example usage: When you want an AI agent to schedule meetings or create calendar events
+# based on conversation analysis or task requirements.
+
+class GoogleCalendarCreateEventAction < Sublayer::Actions::Base
+  def initialize(title:, start_time:, end_time:, description: nil, attendees: [], calendar_id: 'primary')
+    @title = title
+    @start_time = start_time
+    @end_time = end_time
+    @description = description
+    @attendees = attendees
+    @calendar_id = calendar_id
+    
+    @service = Google::Apis::CalendarV3::CalendarService.new
+    @service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open(ENV['GOOGLE_CALENDAR_CREDENTIALS']),
+      scope: Google::Apis::CalendarV3::AUTH_CALENDAR
+    )
+  end
+
+  def call
+    begin
+      event = build_event
+      result = @service.insert_event(@calendar_id, event)
+      
+      Sublayer.configuration.logger.log(:info, "Created calendar event: #{result.id}")
+      result.id
+    rescue Google::Apis::Error => e
+      error_message = "Error creating Google Calendar event: #{e.message}"
+      Sublayer.configuration.logger.log(:error, error_message)
+      raise StandardError, error_message
+    end
+  end
+
+  private
+
+  def build_event
+    Google::Apis::CalendarV3::Event.new(
+      summary: @title,
+      description: @description,
+      start: {
+        date_time: @start_time.iso8601,
+        time_zone: 'UTC'
+      },
+      end: {
+        date_time: @end_time.iso8601,
+        time_zone: 'UTC'
+      },
+      attendees: @attendees.map { |email| { email: email } },
+      reminders: {
+        use_default: true
+      }
+    )
+  end
+end
